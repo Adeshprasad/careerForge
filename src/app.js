0822233ee6a455ab581ad2a026060411db4daf76
Adesh Prasad <adeshprasad010@gmail.com>
2026-06-22 17:56:17 +0530

    Add initial API routes

--- src/app.js
+++ src/app.js
@@ -1,19 +1,20 @@
 const express = require("express");
 
 const app = express();
+const applications = [
+    {
+        company: "Amazon",
+        status: "Applied"
+    },
+    {
+        company: "Google",
+        status: "Interview"
+    }
+];
 
-app.get("/", (req, res) => {
-    res.send("Home Page");
+app.get("/applications", (req,res) => {
+    res.send(applications);
 });
-
-app.get("/profile", (req, res) => {
-    res.send("Profile Page");
-});
-
-app.get("/applications", (req, res) => {
-    res.send("Applications Page");
-});
-
 app.listen(3000, () => {
     console.log("Server running");
 });
